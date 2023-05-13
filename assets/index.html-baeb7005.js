7e868912e7e1e0f32834aeaf207989aebb75a200
AlexChen68 <1274812218@qq.com>
2023-05-13 14:31:12 +0000

    Deploying to gh-pages from @ AlexChen68/AlexChen68.github.io@75741a463bb3df77dad3d456ba2be5d5a9ea11aa 🚀

---
+++ assets/index.html-baeb7005.js
@@ -0,0 +1,6 @@
+import{_ as n,W as e,X as t,a2 as o,Y as a,a0 as l}from"./framework-d3200c61.js";const r={},s=a("h2",{id:"java-简介",tabindex:"-1"},[a("a",{class:"header-anchor",href:"#java-简介","aria-hidden":"true"},"#"),l(" Java 简介")],-1),i=a("p",null,"Java 是由 Sun Microsystems 公司于 1995 年 5 月推出的 Java 面向对象程序设计语言和 Java 平台的总称。由 James Gosling 和同事们共同研发，并在 1995 年正式推出。",-1),v=a("p",null,"后来 Sun 公司被 Oracle（甲骨文）公司收购，Java 也随之成为 Oracle 公司的产品。",-1),J=a("p",null,"Java 分为三个体系：",-1),d=a("ul",null,[a("li",null,"JavaSE(J2SE)(Java2 Platform Standard Edition，java 平台标准版)"),a("li",null,"JavaEE(J2EE)(Java 2 Platform,Enterprise Edition，java 平台企业版)"),a("li",null,"JavaME(J2ME)(Java 2 Platform Micro Edition，java 平台微型版)")],-1),c=a("p",null,'2005 年 6 月，JavaOne 大会召开，SUN 公司公开 Java SE 6。此时，Java 的各种版本已经更名，以取消其中的数字 "2"：J2EE 更名为 Java EE，J2SE 更名为 Java SE，J2ME 更名为 Java ME。',-1);function _(E,m){return e(),t("div",null,[s,i,v,J,d,c,o(` ## Java 基础导航
+
+- [Java 基础语法](grammar.md)
+- [Java String](string.md)
+- [Java 注解机制](annotation.md)
+- [Java 流程控制](flow-control.md) `)])}const h=n(r,[["render",_],["__file","index.html.vue"]]);export{h as default};
